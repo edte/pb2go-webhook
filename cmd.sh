93c17774928ad4bef6b98e9726dd4fc533ee48c7
edte <zzzzip6@gmail.com>
2022-09-08 17:55:55 +0800

    feat(*): project init

--- cmd.sh
+++ cmd.sh
@@ -8,26 +8,43 @@ func judge() {
 }
 
 function init() {
+    echo "begin init"
     go install github.com/rookie-ninja/rk/cmd/rk@latest
     rk install buf
     rk install protobuf
     rk install protoc-gen-go
+    echo "end init"
 }
 
+echo "begin judge.."
 judge()
+echo "end judge"
 
-
+echo "enter $PB_SRC_DIR"
 cd $PB_SRC_DIR
+
+echo "begin build pb"
+
 git pull
 
 buf generate
 
+echo "build pb succ"
+
+echo "enter $PB_GO_DIR"
+
 cd $PB_GO_DIR
 
+echo "cp go to api project"
+
 cp /tmp/api/* .
+
+echo "begin push api project"
 
 git add *
 
 git commit -m "feat(*): update"
 
 git push
+
+echo "update succ"
